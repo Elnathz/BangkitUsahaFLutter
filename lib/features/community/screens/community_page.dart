3b718426d0e92a5d8254dfe813f3db795a51ca51
Elnath <farrosrifantiarno32@gmail.com>
2026-01-16 13:00:00 +0700

    fix: floating action buttom padding

--- lib/features/community/screens/community_page.dart
+++ lib/features/community/screens/community_page.dart
@@ -154,7 +154,7 @@ class _CommunityPageState extends State<CommunityPage>
       floatingActionButton: Tooltip(
         message: 'Buat Postingan',
         child: Padding(
-          padding: const EdgeInsets.only(bottom: 100),
+          padding: const EdgeInsets.only(bottom: 25, right: 10),
           child: Container(
             decoration: BoxDecoration(
               gradient: const LinearGradient(
